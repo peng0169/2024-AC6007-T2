9dfea443f5cfc6019a8a8ef2f3a1d2ee6e0dd7c1
peng0169 <peng0169@e.ntu.edu.sg>
2024-11-20 02:30:14 +0000

    1

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
+# flask is a framework of back end programing: a library
+# a way to simplified the code
+# pip install: python installed has a small library, and the other library is in the internet
+# using install to introduce the library
+# front -> website -> DMS -> router -> ISP -> internet -> back end (app.py)
+# the protocal is call TCP/IP
+# python use WSGI to convert to TCP/IP
+# gunicorn.py help using the the WSGI protocal
 from flask import Flask
 from flask import render_template,request
 
