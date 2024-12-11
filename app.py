f2c847f2fc555d5f4cbc970e2842d6e6c3e31938
peng0169 <peng0169@e.ntu.edu.sg>
2024-12-11 03:41:40 +0000

    1

--- app.py
+++ app.py
@@ -6,14 +6,26 @@
 # the protocal is call TCP/IP
 # python use WSGI to convert to TCP/IP
 # gunicorn.py help using the the WSGI protocal
+# install python, pip install flask, 
 from flask import Flask
 from flask import render_template,request
+import textblob
 
 app = Flask(__name__)
 
 @app.route("/",methods=["GET","POST"])
 def index():
     return(render_template("index.html"))
+
+@app.route("/SA",methods=["GET","POST"])
+def SA():
+    return(render_template("SA.html"))
+
+@app.route("/SA_result",methods=["GET","POST"])
+def SA_result():
+    q = request.form.get("q")
+    r = textblob.TextBlob(q).sentiment
+    return(render_template("SA_result.html", r = r))
 
 if __name__ == "__main__":
     app.run()
